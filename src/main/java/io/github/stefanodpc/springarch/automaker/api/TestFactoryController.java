37083086a18ac7e084b2a54231c40f952595b452
Stefano Carraro <stefano.c@edu.pucrs.br>
2024-12-07 18:28:48 -0300

    Added qualifier annotation to select the intended bean when doing DI

--- src/main/java/io/github/stefanodpc/springarch/automaker/api/TestFactoryController.java
+++ src/main/java/io/github/stefanodpc/springarch/automaker/api/TestFactoryController.java
@@ -1,6 +1,7 @@
 package io.github.stefanodpc.springarch.automaker.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,12 +16,16 @@ import io.github.stefanodpc.springarch.automaker.HondaHRV;
 @RequestMapping("cars")
 public class TestFactoryController {
 
+    // @Qualifier defines the bean being used
+    // Alternatively changing the variable name also defines it
+
     @Autowired
-    private Engine electricEngine;
+    @Qualifier("electricEngine")
+    private Engine engine;
     
     @PostMapping
     public CarStatus startCar(@RequestBody CarKey carKey){
-        var car = new HondaHRV(electricEngine);
+        var car = new HondaHRV(engine);
         return car.startIgnition(carKey);
     }
 }
