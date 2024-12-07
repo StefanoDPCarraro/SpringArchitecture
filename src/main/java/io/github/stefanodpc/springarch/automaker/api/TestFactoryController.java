fffce560e89b4b1f901e0d6f4096386b0ddcb738
Stefano Carraro <stefano.c@edu.pucrs.br>
2024-12-07 18:18:38 -0300

    Added test factory with autowired engine

--- src/main/java/io/github/stefanodpc/springarch/automaker/api/TestFactoryController.java
+++ src/main/java/io/github/stefanodpc/springarch/automaker/api/TestFactoryController.java
@@ -3,6 +3,7 @@ package io.github.stefanodpc.springarch.automaker.api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.github.stefanodpc.springarch.automaker.CarKey;
@@ -11,6 +12,7 @@ import io.github.stefanodpc.springarch.automaker.Engine;
 import io.github.stefanodpc.springarch.automaker.HondaHRV;
 
 @RestController
+@RequestMapping("cars")
 public class TestFactoryController {
 
     @Autowired
