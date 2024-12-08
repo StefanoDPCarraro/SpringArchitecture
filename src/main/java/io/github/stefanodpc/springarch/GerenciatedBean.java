da71f3b3f07dfc43b34345ed05caec97658aeb4b
Stefano Carraro <stefano.c@edu.pucrs.br>
2024-12-08 15:40:17 -0300

    Added scope definition for bean

--- src/main/java/io/github/stefanodpc/springarch/GerenciatedBean.java
+++ src/main/java/io/github/stefanodpc/springarch/GerenciatedBean.java
@@ -1,12 +1,30 @@
 package io.github.stefanodpc.springarch;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import io.github.stefanodpc.springarch.todos.TodoEntity;
 import io.github.stefanodpc.springarch.todos.TodoValidator;
 
+// Every bean has Singleton scope as default
+// It can be changed with the @Scope annotation
+// Scope types:
+// Application = Exists for the application runtime.
+// Request = Instanciated on a request and close at its end.
+// Session = Exists while the user session exists.
+// Singleton = Only one instance, every user uses the same instance.
+// Prototype = One instance for each usage
+
+// BeanDefinition - WebApplicationContext
+
+// Application holds state for every user, session holds for one user.
+// Request does not holds state.
+// Singleton should not hold state since it never ends.
+
 @Component
+@Scope(BeanDefinition.SCOPE_SINGLETON)
 public class GerenciatedBean {
     
     // Injection by property
